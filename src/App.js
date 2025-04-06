115fbb3fa25687899e672f63590e7b1c37e69704
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-04-06 16:07:18 +0530

    create home page to import all the component and follow best practics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-import NavBar from "./components/NavBar";
-import HeroSection from "./components/HeroSection";
-import ServicesSection from "./components/Services";
+// import NavBar from "./components/NavBar";
+// import HeroSection from "./components/HeroSection";
+// import ServicesSection from "./components/Services";
+import HomePage from "./pages/HomePage";
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <HeroSection />
-      <ServicesSection />
+      {/* <NavBar />
+      <HeroSection /> */}
+      {/* <ServicesSection /> */}
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import HeroSection from "../components/HeroSection";
+import ServicesSection from "../components/Services";
+
+function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <HeroSection />
+      <ServicesSection />
+    </>
+  );
+}
+
+export default HomePage;
