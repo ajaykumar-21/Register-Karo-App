c5145ae201453ed3b0776aac00d9945a58a853d1
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-04-06 17:19:47 +0530

    add SEO meta title and description using Helmet in home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import NavBar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
 import ServicesSection from "../components/Services";
@@ -7,6 +8,13 @@ import Testimonials from "../components/Testimonials";
 function HomePage() {
   return (
     <>
+      <Helmet>
+        <title>Register Karo - Business Registration Made Easy</title>
+        <meta
+          name="description"
+          content="Simplified legal solutions to get your business up and running effortlessly. Register your company, file GST, and secure trademarks with ease."
+        />
+      </Helmet>
       <NavBar />
       <HeroSection />
       <ServicesSection />
